8a0cd53bce18e0d2854911652549ed2db82af92e
Shahriyordadad <151160287+Shahriyordadad@users.noreply.github.com>
2024-07-29 13:22:35 +0500

    Update settings.py

--- b/settings.py
+++ b/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = env.bool("DEBUG", default=False)
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['127.0.0.1','vercel.app']
 
 
